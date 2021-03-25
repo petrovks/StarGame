190c3e5c4f3a2dfc869fecee31679e794d012bfa
petrovks <konstantin.petrov.87@gmail.com>
2021-03-26 00:54:26 +0300

    Initial commit

---
+++ desktop/src/geekbrains/libgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package geekbrains.libgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import geekbrains.libgdx.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
